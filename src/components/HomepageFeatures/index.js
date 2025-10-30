062b4bd9179722c47c7e94c4f0307c48a3aaff49
Calvin Secrest <dev@calvinsecrest.com>
2025-10-30 23:25:16 +0000

    update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 const FeatureList = [
   {
     title: "Connected Rural Communities",
-    Svg: require("@site/static/img/arginet_mountain.svg").default,
+    Svg: require("@site/static/img/agrinet_mountains.svg").default,
     description: (
       <>
         Agrinet delivers dependable last-mile connectivity so farmers,
